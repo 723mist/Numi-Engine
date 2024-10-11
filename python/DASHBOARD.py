b7d7972896246db1a95941982616b7f6c6786737
MIST <eksperementy300@gmail.com>
2024-10-11 09:24:26 +0300

    11.10.2024 09:24

--- python/DASHBOARD.py
+++ python/DASHBOARD.py
@@ -5,12 +5,39 @@ from pyconf import *
 import functions.centerWindow
 from functions.centerWindow import *
 
+color = '#808080'
+color2 = '#606060'
+
 DASHBOARD = Tk()
 DASHBOARD.title("Py.name.MISTIC_HAMMER")
 DASHBOARD.geometry("602x598")
 DASHBOARD.config(bg="gray")
 DASHBOARD.resizable(False, False)
+DASHBOARD['bg'] = color
 center_window(DASHBOARD)
+
+tkfont = ('Arial', 24)
+
+# стили
+style = tkk.Style()
+style.configure('TButton', font=tkfont)
+
+# создание фрейма (на нем легче все размещать)
+frame = tk.Frame(DASHBOARD, bg=color)
+frame.place(relwidth=1, relheight=1)    
+
+# все папки, которые начинаются на "AntProj_"
+def update_folders():
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    folders = [folder for folder in os.listdir(current_directory) if os.path.isdir(os.path.join(current_directory, folder))]
+    filtered_folders = [folder for folder in folders if folder.startswith('mhle_')]
+
+    listbox.delete(0, tk.END)
+
+    for folder in filtered_folders:
+        display_name = folder.replace('mhle_', '', 1)
+        listbox.insert(tk.END, display_name)
+
 
 def new():
 
@@ -24,12 +51,21 @@ def new():
                 file.write("Create with MISTIC_HAMMER")
             tk_msg.showinfo("Project created, now open engine", f"Project '{proj_name}' created successfully!", parent=DASHBOARD)
 
-            import ENGINE
-            DASHBOARD.destroy()
+            # import ENGINE
+            # DASHBOARD.destroy()
         else:
             tk_msg.showerror("ERROR", "A project with this name already exists!", parent=DASHBOARD)
 
-    
+
+folders_text = tk.Label(frame, text="Your Projects", font=tkfont, bg=color2)
+folders_text.place(relx=0.2, y=225, anchor='n')
+
+listbox = tk.Listbox(frame, width=25, height=10, font=('Arial', 16))
+listbox.place(relx=0.2, y=275, anchor='n')
+
+update_folders()
+
+
 
 button = ttk.Button(text="New", command=new)
 button.place(relx=0.90365, rely=0.01, anchor=NW)
