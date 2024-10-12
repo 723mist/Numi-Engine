47aa07084f3ec2f03d973a7496d4cf2369668dcb
MIST <eksperementy300@gmail.com>
2024-10-12 16:08:26 +0300

    Bugfix 16:08 12.10.2024

--- python/Engine/Engine.py
+++ python/Engine/Engine.py
@@ -1,63 +1,61 @@
 from tkinter import *
 from tkinter import filedialog as fd
 
-def engine_start():
-    def ofd():
+
+def ofd():
         print("ofd = Open file dialog")
         filename = fd.askopenfilename()
 
-    def exit():
-        print("Exit")
-        editor_hammer.destroy()
+def exit():
+    print("Exit")
+    editor_hammer.destroy()
 
-    def config():
-        print("Config open")
-        e_config = Tk()
-        e_config.title("Editor/Hammer config")
-        e_config.geometry("546x256")
-        e_config.config(bg=color)
+def config():
+    print("Config open")
+    e_config = Tk()
+    e_config.title("Editor/Hammer config")
+    e_config.geometry("546x256")
+    e_config.config(bg=color)
 
-    def opendashboard():
-        print("Dashboard open")
-        print("Hammer close")
-        import python.DASHBOARD
-        editor_hammer.destroy()
+def opendashboard():
+    print("Dashboard open")
+    print("Hammer close")
+    import python.DASHBOARD
+    editor_hammer.destroy()
 
-    color = "#252426"
+color = "#252426"
 
-    editor_hammer = Tk()
-    editor_hammer.title("Editor/Hammer")
-    editor_hammer.geometry("1208x640") 
-    editor_hammer.option_add("*tearOff", FALSE)
-    editor_hammer.config(bg=color)
-    #editor_hammer.resizable(width=False, height=False)
+editor_hammer = Tk()
+editor_hammer.title("Editor/Hammer")
+editor_hammer.geometry("1208x640") 
+editor_hammer.option_add("*tearOff", FALSE)
+editor_hammer.config(bg=color)
+#editor_hammer.resizable(width=False, height=False)
     
-    toolBarEditor = Menu()
+toolBarEditor = Menu()
 
-    tools_in_toolbar_options = Menu()
+tools_in_toolbar_options = Menu()
 
-    tools_in_toolbar = Menu()
-    tools_in_toolbar.add_command(label="New")
-    tools_in_toolbar.add_cascade(label="Open", command=ofd)
-    tools_in_toolbar.add_command(label="Save")
-    # tools_in_toolbar.add_cascade(label="Options", menu= tools_in_toolbar_options)
-    tools_in_toolbar.add_separator()
-    tools_in_toolbar.add_cascade(label="Open deshboard", command=opendashboard)
-    tools_in_toolbar.add_cascade(label="Exit", command=exit)
-
-
-    tool_bar_edit =Menu(toolBarEditor, tearoff=0)
-    tool_bar_edit.add_command(label="Copy")
-    # tool_bar_edit.add_command(label="Cut")
-    tool_bar_edit.add_command(label="Past")
-    tool_bar_edit.add_separator()
-    tool_bar_edit.add_command(label="Config", command=config)
-    toolBarEditor.add_cascade(label="File", menu=tools_in_toolbar)
-    toolBarEditor.add_cascade(label="Edit", menu=tool_bar_edit)
-    toolBarEditor.add_cascade(label="View")
-
-    editor_hammer.config(menu=toolBarEditor)
-
-    editor_hammer.mainloop()
+tools_in_toolbar = Menu()
+tools_in_toolbar.add_command(label="New")
+tools_in_toolbar.add_cascade(label="Open", command=ofd)
+tools_in_toolbar.add_command(label="Save")
+# tools_in_toolbar.add_cascade(label="Options", menu= tools_in_toolbar_options)
+tools_in_toolbar.add_separator()
+tools_in_toolbar.add_cascade(label="Open deshboard", command=opendashboard)
+tools_in_toolbar.add_cascade(label="Exit", command=exit)
 
 
+tool_bar_edit =Menu(toolBarEditor, tearoff=0)
+tool_bar_edit.add_command(label="Copy")
+# tool_bar_edit.add_command(label="Cut")
+tool_bar_edit.add_command(label="Past")
+tool_bar_edit.add_separator()
+tool_bar_edit.add_command(label="Config", command=config)
+toolBarEditor.add_cascade(label="File", menu=tools_in_toolbar)
+toolBarEditor.add_cascade(label="Edit", menu=tool_bar_edit)
+toolBarEditor.add_cascade(label="View")
+
+editor_hammer.config(menu=toolBarEditor)
+
+editor_hammer.mainloop()
