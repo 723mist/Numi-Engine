6b0a576f0c2f8283ef40f93e018029ff05b98b09
MISTICMAN23 <132823841+723mist@users.noreply.github.com>
2024-11-19 20:33:51 +0300

    Engine

---
+++ Engine/python/GUI/maingui.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import filedialog as fd
+from tkinter import ttk
+
+# colors
+color1 = "#252426"
+
+# Window
+wengine = Tk()
+wengine.title("Engine")
+wengine.geometry("1024x600")
+wengine.config(bg=color1)
+
+# MenuBar
+
+
+wengine.mainloop()
